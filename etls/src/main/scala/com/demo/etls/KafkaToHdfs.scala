b6c43c73865a5aaebb08f24065a7599f6b041dc4
lihao <lihao@cloud-young.com>
2018-07-20 17:30:17 +0800

    create

--- etls/src/main/scala/com/demo/etls/KafkaToHdfs.scala
+++ etls/src/main/scala/com/demo/etls/KafkaToHdfs.scala
@@ -18,43 +18,48 @@ import org.apache.spark.streaming.kafka.KafkaUtils
   * @version 1.0 2018-7-19 16:56 by 李浩（lihao@cloud-young.com）创建
   */
 object KafkaToHdfs {
-  val conf = new SparkConf().setAppName("KafkaDirectStream").setMaster("local[1]")
-  val ssc = new StreamingContext(conf, Seconds(Config.timeInterval.toInt))
-  val kafkaParams = Map(
-    "zookeeper.connect" -> Config.zkQuorum,
-    "group.id" -> Config.groupId,
-    "metadata.broker.list" -> Config.brokerList,
-    "auto.offset.reset" -> "smallest")
-  val topics = Set(Config.topic)
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("KafkaDirectStream").setMaster("local[1]")
+    val ssc = new StreamingContext(conf, Seconds(Config.timeInterval.toInt))
+    val kafkaParams = Map(
+      "zookeeper.connect" -> Config.zkQuorum,
+      "group.id" -> Config.groupId,
+      "metadata.broker.list" -> Config.brokerList,
+      "auto.offset.reset" -> "smallest")
+    val topics = Set(Config.topic)
 
-  val directKafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics)
+    val directKafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics)
 
-  val config = new Configuration()
-  val fs = FileSystem.get(new URI("hdfs://ubuntu2:9000"),config);
-  var output = "test"
+    val config = new Configuration()
+    val fs = FileSystem.get(new URI("hdfs://master:8020/"), config);
+    var output = "test"
 
-  directKafkaStream.foreachRDD(rdd => {
-    if (!rdd.isEmpty()) {
-      val path = hdfsPath;
-      if (!fs.exists(path)){
-        val fin = fs.create(path)
-        rdd.foreach(item => {
-          fin.writeUTF(item + "\n")
-        })
-      }else{
-        val fin = fs.append(path)
-        rdd.foreach(item => {
-          fin.writeUTF(item + "\n")
+    directKafkaStream.foreachRDD(rdd => {
+      if (!rdd.isEmpty()) {
+        val path = hdfsPath;
+        val pathUrl = path.toUri
+
+        if (!fs.exists(path)) {
+          fs.create(path)
+        }
+
+        rdd.foreachPartition(it => {
+          val configP = new Configuration()
+          val fsP = FileSystem.get(new URI("hdfs://master:8020/"), configP);
+          val fin = fsP.append(new Path(pathUrl))
+          for (v <- it) {
+            fin.writeUTF(v._2 + "\n")
+          }
         })
       }
+    })
+
+    ssc.start()
+    ssc.awaitTermination()
+
+    def hdfsPath = {
+      import java.util.Calendar
+      new Path(output + "/" + Calendar.getInstance.get(Calendar.YEAR) + "-" + (Calendar.getInstance.get(Calendar.MONTH) + 1) + "-" + Calendar.getInstance.get(Calendar.DATE) + "/" + Calendar.getInstance.get(Calendar.YEAR) + "-" + (Calendar.getInstance.get(Calendar.MONTH) + 1) + "-" + Calendar.getInstance.get(Calendar.DATE) + ".txt")
     }
-  })
-
-  ssc.start()
-  ssc.awaitTermination()
-
-  def hdfsPath = {
-    import java.util.Calendar
-    new Path(output + "/" + Calendar.getInstance.get(Calendar.YEAR) + "-" + (Calendar.getInstance.get(Calendar.MONTH) + 1) + "-" + Calendar.getInstance.get(Calendar.DATE) + "/" + Calendar.getInstance.get(Calendar.YEAR) + "-" + (Calendar.getInstance.get(Calendar.MONTH) + 1) + "-" + Calendar.getInstance.get(Calendar.DATE) + ".txt")
   }
 }
